2a102dfc0ffa815f0b2ce951f780efbbc6b91f8b
syongk69 <113334586+syongk69@users.noreply.github.com>
2023-08-21 16:45:53 -0700

    manually uploaded

---
+++ db/client.js
@@ -0,0 +1,14 @@
+const { Client } = require("pg");
+require("dotenv").config();
+
+const connectionString = process.env.DATABASE_URL;
+
+const client = new Client({
+  connectionString,
+  ssl:
+    process.env.NODE_ENV === "production"
+      ? { rejectUnauthorized: false }
+      : undefined,
+});
+
+module.exports = client;
